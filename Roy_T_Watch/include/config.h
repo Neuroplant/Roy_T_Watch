6419a224342bf4cef282057069df607809d60123
Neuroplant <m.roych@gmail.com>
2020-09-21 09:24:49 +0200

    Add files via upload

--- Roy_T_Watch/include/config.h
+++ Roy_T_Watch/include/config.h
@@ -1,7 +1,7 @@
 // => Hardware select
 // #define LILYGO_WATCH_2019_WITH_TOUCH     // To use T-Watch2019 with touchscreen, please uncomment this line
 // #define LILYGO_WATCH_2019_NO_TOUCH          // To use T-Watch2019 Not touchscreen , please uncomment this line
- #define LILYGO_WATCH_2020_V1             //To use T-Watch2020, please uncomment this line
+#define LILYGO_WATCH_2020_V1             //To use T-Watch2020, please uncomment this line
 
 
 // NOT SUPPORT ...
@@ -11,6 +11,14 @@
 // => Function select
 #define LILYGO_WATCH_LVGL                   //To use LVGL, you need to enable the macro LVGL
 
+
+
+
 #include <LilyGoWatch.h>
 
 
+// Personal Configuration
+
+#define SLEEP_TIMER 20			//comment to enable Sleep after 20s
+#define DIGITAL_1				//comment to enable Digital Watchface
+#define ANALOG_1				//comment to enable Analog Watchface
